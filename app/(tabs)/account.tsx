917a2d2e290bb7133ab68ca337fe375379cf8780
David Henao <daves.hb@hotmail.com>
2026-01-08 12:56:40 -0500

    first commit

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { ActivityIndicator, Appbar, MD2Colors } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -20,26 +20,18 @@ export default function HomeScreen() {
     <>
       <SafeAreaView style={{ flex: 1 }}>
         <Appbar.Header style={styles.header}>
-          <Appbar.Content title="1234" subtitle={"Subtitle"} />
+          <Appbar.Content title="Menu" subtitle={"Subtitle"} />
           <Appbar.Action icon="magnify" onPress={() => {}} />
           <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
         </Appbar.Header>
 
         <View style={styles.container}>
-          <Text style={styles.title}>hola mundo </Text>
-          <Text style={styles.title}>El contador es: </Text>
-          <Text style={styles.title}>{count}</Text>
+      
 
-          <Pressable
-            onPress={increment}
-            onLongPress={reset}
-            style={({ pressed }) => [
-              styles.primaryButton,
-              pressed && styles.primaryButtonPressed,
-            ]}
-          >
-            <Text style={{ color: "#ffffff" }}> pressable!</Text>
-          </Pressable>
+         
+            <Text style={{ color: "#19a92cff", fontSize: 60
+             }}> Account!</Text>
+        
 
           <ActivityIndicator animating={true} color={MD2Colors.red800} />
 
@@ -88,27 +80,3 @@ const styles = StyleSheet.create({
   },
 });
 
-// const styles = StyleSheet.create({
-
-// container:{
-//   // backgroundColor: "green",
-//   flex:1
-// },
-
-//   titleContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     gap: 8,
-//   },
-//   stepContainer: {
-//     gap: 8,
-//     marginBottom: 8,
-//   },
-//   reactLogo: {
-//     height: 178,
-//     width: 290,
-//     bottom: 0,
-//     left: 0,
-//     position: 'absolute',
-//   },
-// });
--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -1,12 +1,14 @@
 // Fallback for using MaterialIcons on Android and web.
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { SymbolWeight, SymbolViewProps } from 'expo-symbols';
+import { SymbolViewProps, SymbolWeight } from 'expo-symbols';
 import { ComponentProps } from 'react';
 import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
 
 type IconMapping = Record<SymbolViewProps['name'], ComponentProps<typeof MaterialIcons>['name']>;
 type IconSymbolName = keyof typeof MAPPING;
+
+// import GroupIcon from '@mui/icons-material/Group';
 
 /**
  * Add your SF Symbols to Material Icons mappings here.
@@ -16,6 +18,7 @@ type IconSymbolName = keyof typeof MAPPING;
 const MAPPING = {
   'house.fill': 'home',
   'paperplane.fill': 'send',
+  // 'user':'power',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
 } as IconMapping;
